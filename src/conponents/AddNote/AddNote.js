8e8b1e78699e2a836d084fbc387dd7ef029b05e2
sujon-chambugong <sujonchambugong@gmail.com>
2022-09-18 19:48:39 +0600

    create button created

--- src/conponents/AddNote/AddNote.js
+++ src/conponents/AddNote/AddNote.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import MyNote from '../MyNote/MyNote';
 import './AddNote.css';
 
 const NoteField = () => {
     return (
         <div>
-            <h1>Note Field</h1>
-            <textarea name="" id="my-text" cols="30" rows="10" placeholder='write something here'></textarea>
-            <br />
-            <button className='add-note'>add note</button>
+            <div className='add-container'>
+                <h1>Add Note</h1>
+                <textarea name="" id="my-text" cols="30" rows="10" placeholder='write something here'></textarea>
+                <br />
+                <button className='add-note'>add note</button>
+            </div>
+            <MyNote />
         </div>
     );
 };
---
+++ src/conponents/MyNote/MyNote.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './MyNote.css';
+
+const MyNote = () => {
+    return (
+        <div className='my-container'>
+            <h1>My Notes</h1>
+            <div className='notes'>
+                <div className='mynote'>
+                    <p>This is my note and tommorow, I am going submit this assigment</p>
+                    <div>
+                        <button className='noteBtn'>edit</button>
+                        <button className='noteBtn'>del</button>
+                    </div>
+                </div>
+                <div className='mynote'>
+                    <p>This is my note and tommorow, I am going submit this assigment</p>
+                    <div>
+                        <button className='noteBtn'>edit</button>
+                        <button className='noteBtn'>del</button>
+                    </div>
+                </div>
+                <div className='mynote'>
+                    <p>This is my note and tommorow, I am going submit this assigment</p>
+                    <div>
+                        <button className='noteBtn'>edit</button>
+                        <button className='noteBtn'>del</button>
+                    </div>
+                </div>
+                <div className='mynote'>
+                    <p>This is my note and tommorow, I am going submit this assigment</p>
+                    <div>
+                        <button className='noteBtn'>edit</button>
+                        <button className='noteBtn'>del</button>
+                    </div>
+                </div>
+                <div className='mynote'>
+                    <p>This is my note and tommorow, I am going submit this assigment</p>
+                    <div>
+                        <button className='noteBtn'>edit</button>
+                        <button className='noteBtn'>del</button>
+                    </div>
+                </div>
+                <div className='mynote'>
+                    <p>This is my note and tommorow, I am going submit this assigment</p>
+                    <div>
+                        <button className='noteBtn'>edit</button>
+                        <button className='noteBtn'>del</button>
+                    </div>
+                </div>
+                <div className='mynote'>
+                    <p>This is my note and tommorow, I am going submit this assigment</p>
+                    <div>
+                        <button className='noteBtn'>edit</button>
+                        <button className='noteBtn'>del</button>
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+    );
+};
+
+export default MyNote;
